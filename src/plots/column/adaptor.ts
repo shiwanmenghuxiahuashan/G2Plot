2910b276b21683b8baab70390ca44887a6bda524
visiky <736929286@qq.com>
2022-01-23 19:44:12 +0800

    fix(issue-3009): x-scale 为 continuous data 时，指定 type 为 cat 会导致绘制出错

--- src/plots/column/adaptor.ts
+++ src/plots/column/adaptor.ts
@@ -140,7 +140,7 @@ export function meta(params: Params<ColumnOptions>): Params<ColumnOptions> {
       },
       {
         [xField]: {
-          type: 'cat',
+          // type: 'cat',
         },
         [yField]: {
           ...adjustYMetaByZero(data, yField),
